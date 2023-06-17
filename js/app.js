f1e006a36611abffed0e926345160f947df6a150
thewa1818 <zawa.i.0218@gmail.com>
2023-06-17 20:38:41 +0900

    4

--- js/app.js
+++ js/app.js
@@ -13,9 +13,30 @@ $("#btn").on("click",function(){
 
   $(".bmi_total").fadeIn();
 
+  $("#bmi_text").fadeIn();
+
   $("#bmi_kekka").text(bmi);
 
  localStorage.setItem(d,bmi);
+
+ const level = ["痩せすぎだよ","適正体重！","ぽっちゃりさん","もう食うなデブ！"]
+
+// console.log(level);
+
+ if (bmi <= 19){
+  console.log(level[0]);
+  $("#bmi_text").text("痩せすぎだよ");
+ }else if(bmi < 25 && bmi >=19){
+  console.log(level[1]);
+  $("#bmi_text").text("適正体重！");
+ }else if(bmi < 35 && bmi >=25){
+  console.log(level[2]);
+  $("#bmi_text").text("ぽっちゃりさん");
+ }else{
+  console.log(level[3]);
+  $("#bmi_text").text("もう食うなデブ！");}
+
+
 
  const html = `
 <tr>
@@ -30,10 +51,12 @@ $("#list").append(html);
 $("#btn2").on("click",function(){
   $("#weight,#tall,#day").val("");
   $(".bmi_total").hide();
+  $("#bmi_text").hide();
 });
 
 $("#btn3").on("click",function(){
   localStorage.clear();
-  $("#list").remove();
-  $(".bmi_total").remove();
+  $("#list").hide();
+  $(".bmi_total").hide();
+  $("#bmi_text").hide();
 });
